8685edd673081121802f4ad782f9f531a310fdc5
mjaheer <mjaheer1@in.ibm.com>
2019-07-31 16:08:40 +0530

    Initial commit

---
+++ src/main/java/com/stackroute/microservices/CentralizedConfigService/CentralizedConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.microservices.CentralizedConfigService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralizedConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralizedConfigServiceApplication.class, args);
+	}
+
+}
